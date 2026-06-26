5fb1eaaa8ed72364594554b8e7ede6392f3c1a05
Ansh Jaiswal <jaiswalanshv2@gmail.com>
2026-06-26 15:29:30 +0530

    feat: deploy ProbableMarket to Sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const { PRIVATE_KEY, RPC_URL } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: RPC_URL || "",
+      accounts: PRIVATE_KEY ? [`0x${PRIVATE_KEY}`] : [],
+      chainId: 11155111,
+    },
+  },
 };
